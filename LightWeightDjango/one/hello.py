5507407e297b4b5fc9fe044e911091c380615b1e
avery <wuqize5109@qq.com>
2017-06-18 09:52:06 +0800

    one

---
+++ LightWeightDjango/one/hello.py
@@ -0,0 +1,46 @@
+#coding=utf-8
+"""
+视图 -> URL -> 设置 -> 运行
+
+"""
+
+
+import os
+import sys
+
+from django.conf import settings
+
+DEBUG = os.environ.get('DEBUG', 'on') == 'on'
+SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(32))
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+
+settings.configure(
+    DEBUG=DEBUG,
+    SECRET_KEY=SECRET_KEY,
+    ALLOWED_HOSTS=ALLOWED_HOSTS,
+    ROOT_URLCONF=__name__,
+    MIDDLEWARE_CLASSES=(
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    )
+)
+
+from django.conf.urls import url
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Hello World!')
+
+urlpatterns = (
+    url( r'^$', index),
+)
+
+from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
+
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
